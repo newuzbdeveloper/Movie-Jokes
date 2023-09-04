684776218e71aea45888b77e4347ef7e85360068
newuzbdeveloper <106350805+newuzbdeveloper@users.noreply.github.com>
2023-09-04 02:02:12 +0200

    Improvements by Redux.

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,16 +1,42 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import MovieCard from "./MovieCard";
-import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Heading, SimpleGrid, Spinner, useToast } from "@chakra-ui/react";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
-import { selectedMovies } from "./movieSlice";
+import {
+  fetchMovies,
+  selectedMovies,
+  selectedMoviesError,
+  selectedMoviesStatus,
+} from "./movieSlice";
 
 function MoviesList() {
   const movies = useSelector(selectedMovies);
-  return (
-    <>
-      <Heading size="xl" textAlign="center" pb={4}>
-        Trending Movies.
-      </Heading>
+  const moviesStatus = useSelector(selectedMoviesStatus);
+  const moviesError = useSelector(selectedMoviesError);
+  const dispatch = useDispatch();
+  const toast = useToast();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchMovies()).unwrap();
+      } catch (err) {
+        toast({
+          title: "Failed to load movies.",
+          description: "Please check your internet connection.",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    };
+    fetchData();
+  }, [dispatch, toast]);
+
+  let content;
+  if (moviesStatus === "succeeded") {
+    content = (
       <SimpleGrid columns={{ sm: 1, md: 3, lg: 4 }} spacing={4}>
         {movies.map((movie) => (
           <MovieCard
@@ -22,6 +48,24 @@ function MoviesList() {
           />
         ))}
       </SimpleGrid>
+    );
+  } else if (moviesStatus === "loading") {
+    content = (
+      <Flex alignItems="center" justifyContent="center" minH="100vh">
+        <Spinner text="Loading..." />
+      </Flex>
+    );
+  } else if (moviesStatus === "failed") {
+    <Flex alignItems="center" bg="red" justifyContent="center" minH="100vh">
+      {moviesError}
+    </Flex>;
+  }
+  return (
+    <>
+      <Heading size="xl" textAlign="center" pb={4}>
+        Trending Movies.
+      </Heading>
+      {content}
     </>
   );
 }
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,13 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { data } from "./data";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchPopularMovies } from "./api";
+
+const initialState = {
+  movies: [],
+  status: "idle",
+  error: null,
+};
+
+export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
+  const data = await fetchPopularMovies();
+  return data.data;
+});
 
 const moviesSlice = createSlice({
   name: "movies",
-  initialState: data.results,
+  initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMovies.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.movies = action.payload.results;
+      })
+      .addCase(fetchMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
 
-export const selectedMovies = (state) => state.movies;
+export const selectedMovies = (state) => state.movies.movies;
+export const selectedMoviesStatus = (state) => state.movies.status;
+export const selectedMoviesError = (state) => state.movies.error;
 export const selectedMovieId = (state, movieId) =>
-  state.movies.find((movie) => movie.id === Number(movieId));
+  state.movies.movies.find((movie) => movie.id === Number(movieId));
 export default moviesSlice.reducer;
